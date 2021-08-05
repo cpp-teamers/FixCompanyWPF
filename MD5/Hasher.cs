541a364bd2f0d34e0e02b383a0086c5cb1a95f0d
MrZend <vlad04102002@gmail.com>
2021-08-05 18:51:07 +0300

    add: MD5 hasher

---
+++ MD5/Hasher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace MD5Library
+{
+    class Hasher
+    {
+        static string GetHash(string input)
+        {
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
