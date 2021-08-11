ff5d9db59fe6623f015a6021a6d3e8df12397667
KhomenkoMac <maksim_khomenko@ukr.net>
2021-08-11 17:11:32 +0300

    add: appropriate fields to Message-class

--- EntityLibrary/Models/Message.cs
+++ EntityLibrary/Models/Message.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EntityLibrary.Models
 {
-	class Message
+	[Table("Messages")]
+	public class Message
 	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		[Column(TypeName = "bit")]
+		public bool HasRead { get; set; }
+
+		[Required]
+		[Column(TypeName = "datetime")]
+		public DateTime TimeStamp { get; set; }
+
+		[Required]
+		[StringLength(250)]
+		public string Content { get; set; }
 	}
 }
