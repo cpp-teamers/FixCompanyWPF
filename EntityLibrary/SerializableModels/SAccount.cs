0912ad68c01b8bfa693a0ec13cea5abf708ce61f
Student <student@lan.itstep.kiev.ua>
2021-08-11 19:03:40 +0300

    add: basic Server and basic TcpClient in Manager Module

---
+++ EntityLibrary/SerializableModels/SAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLibrary.SerializableModels
+{
+    [Serializable]
+    public class SAccount
+    {
+        public SAccount() { }
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+    }
+}
